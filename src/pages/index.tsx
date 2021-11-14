7f91f39d55a95f901e70c6b2f212a7d22602ab80
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-14 03:41:47 -0500

    Instance strengths modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,22 @@ import { useQuery } from 'react-query'
 import { fetchUserBio } from 'api/fetchUserBio'
 import { Column } from 'components'
 import { ProfileHeader } from 'components/Profile/ProfileHeader'
-import { Divider } from '@chakra-ui/react'
+import { Divider, useDisclosure } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import { Strengths } from 'components/Strengths'
+import { StrengthModal, Strengths } from 'components/Strengths'
+import { useState } from 'react'
+import { Strength } from 'types/strength'
 
-const MapStrengths = ({ strengths }: { strengths: any }) => {
+type MapStrengthsProps = {
+  strengths: any
+  onSelectStrength: any
+}
+const MapStrengths = ({ strengths, onSelectStrength }: MapStrengthsProps) => {
   const strengthLabels = [
     { label: 'Master / Influencer', key: 'master' },
     { label: 'Expert', key: 'expert' },
     { label: 'Proficient', key: 'proficient' },
+    { label: 'Novice', key: 'novice' },
     { label: 'No experience, but interested', key: 'no-experience-interested' },
   ]
 
@@ -22,6 +29,7 @@ const MapStrengths = ({ strengths }: { strengths: any }) => {
             <Strengths
               proficiencyLabel={strength.label}
               strengths={strengths?.[strength.key]}
+              onSelectStrength={onSelectStrength}
             />
           )
       )}{' '}
@@ -29,17 +37,34 @@ const MapStrengths = ({ strengths }: { strengths: any }) => {
   )
 }
 const Home: NextPage = () => {
-  const username = 'torrenegra'
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [selectedStrength, setSelectedStrength] = useState<Strength>()
+
+  const username = 'upeguiborja'
   const { data: dataUserBio } = useQuery(['bio', username], fetchUserBio)
 
+  const handleSelectStrength = (strength: any) => {
+    console.log(strength)
+    setSelectedStrength(strength)
+    onOpen()
+  }
+
   return (
-    <Column>
-      <ProfileHeader person={dataUserBio?.person} />
-      <Divider mt='16px' />
-      <MapStrengths
-        strengths={dataUserBio?.extra?.strengthsByProficiency}
-      ></MapStrengths>
-    </Column>
+    <>
+      <Column>
+        <ProfileHeader person={dataUserBio?.person} />
+        <Divider mt='16px' />
+        <MapStrengths
+          strengths={dataUserBio?.extra?.strengthsByProficiency}
+          onSelectStrength={handleSelectStrength}
+        ></MapStrengths>
+      </Column>
+      <StrengthModal
+        isOpen={isOpen}
+        onClose={onClose}
+        strength={selectedStrength}
+      />
+    </>
   )
 }
 
