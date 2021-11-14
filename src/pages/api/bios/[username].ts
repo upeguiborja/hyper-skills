0dfb5671c96f4e39d4d470de489114a0efb5f5c1
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-14 00:53:32 -0500

    Better error handing on serverless proxy function

--- src/pages/api/bios/[username].ts
+++ src/pages/api/bios/[username].ts
@@ -1,12 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import axios from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
   const { username } = req.query
 
   await axios
     .get(`https://bio.torre.co/api/bios/${username}`)
-    .then(({ data }) => {
+    .then((response: AxiosResponse) => {
+      const { data } = response
       const strengthsByProficiency = data?.strengths?.reduce(
         (prev: any, current: any) => {
           return prev[current.proficiency]
@@ -18,10 +19,15 @@ const handler = async (req: VercelRequest, res: VercelResponse) => {
         },
         []
       )
-      res.send({
+      res.json({
         ...data,
         extra: { strengthsByProficiency: strengthsByProficiency },
       })
+    })
+    .catch((error: AxiosError) => {
+      if (error.response?.status) {
+        res.status(error.response.status).json({ error: error.response.data })
+      }
     })
 }
 
