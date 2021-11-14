0b6bcdc22a0fa1c408f60a925707dd111075f3cb
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-14 00:39:28 -0500

    Enhance eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'next/core-web-vitals',
   ],
+  overrides: [{ files: ['*.ts', '*.tsx'], rules: { 'no-undef': 'off' } }],
 }
