6a1321a97e77c966e9efd2b0d046c62164616967
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-13 22:18:39 -0500

    Add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
