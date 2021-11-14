23eb363b13515285f73fab6a1d57249d25f893a9
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-14 03:15:06 -0500

    Add head metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'theme'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Head from 'next/head'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,12 +15,18 @@ const queryClient = new QueryClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Hyper Profile</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   )
 }
 
