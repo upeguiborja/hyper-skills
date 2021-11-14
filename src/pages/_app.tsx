5eb6156840f620dd88ae95d61e859e6236ac23c0
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-11-14 02:05:48 -0500

    Update react-query defaults

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,13 @@ import { theme } from 'theme'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
